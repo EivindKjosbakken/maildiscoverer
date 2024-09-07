e2d19de84baa227b4300a1cc2fc91171a6c37f8c
Eivind Kjosbakken <eivindkjosbakken@gmail.com>
2024-09-07 17:59:14 +0200

    fixing auth to work in prod

--- utility.py
+++ utility.py
@@ -59,14 +59,12 @@ def authorize_gmail_api():
         if creds and creds.expired and creds.refresh_token:
           creds.refresh(Request())
         else:
-          # flow = InstalledAppFlow.from_client_secrets_file(
-          #     "credentials.json", SCOPES
-          # )
           flow = InstalledAppFlow.from_client_config(
               CLIENT_CONFIG, SCOPES
           )
           
-          creds = flow.run_local_server(port=8080)
+          # creds = flow.run_local_server(port=8080)
+          creds = flow.run_console()
         # Save the credentials for the next run
         with open("token.json", "w") as token: 
           token.write(creds.to_json())
