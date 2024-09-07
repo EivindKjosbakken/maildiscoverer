3d14eabd062b3df435343fb1d8ca601835a9dda4
Eivind Kjosbakken <eivindkjosbakken@gmail.com>
2024-09-07 17:54:20 +0200

    add safe constants (not sensitive info)

--- safe_constants.py
+++ safe_constants.py
@@ -1,1 +1,8 @@
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly", "https://www.googleapis.com/auth/userinfo.email", "openid"]
+
+PROJECT_ID = "maildiscoverer"
+AUTH_URI = "https://accounts.google.com/o/oauth2/auth"
+TOKEN_URI = "https://oauth2.googleapis.com/token"
+AUTH_PROVIDER_X509_CERT_URL = "https://www.googleapis.com/oauth2/v1/certs"
+
+MAX_CHARACTER_LENGTH_EMAIL = 12000
